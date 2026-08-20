23f70db700fe8b85d064f31c5bb37d9d500bdff8
Magnus Thor Torfason <m@zulutime.net>
2026-08-20 17:18:01 +0000

    Fix write_fwf() handling of column name widths

--- R/write_fwf.R
+++ R/write_fwf.R
@@ -2,7 +2,9 @@
 write_fwf <- function(d, file) {
 
   # Determine column widths: max of column name and max string in column + 2
-  col_widths <- sapply(d, \(x){max(nchar(x))+2})
+  col_widths_names  <- nchar(names(d))
+  col_widths_values <- sapply(d, \(x) max(nchar(x)))
+  col_widths <- pmax(col_widths_names, col_widths_values) + 2
 
   # Create format string for sprintf
   fmt <- paste0(mapply(function(w) paste0("%-", w, "s"), col_widths), collapse = "")
